11c9b9744fac6f7d5c0626b4f027cc50bfcd3506
Viviane Viana Bresolin <vivianebresolin@gmail.com>
2023-06-18 17:25:05 -0400

    Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,60 @@ import {
   ClerkProvider, 
   SignedIn,
   SignedOut,
-  RedirectToSignIn, } from "@clerk/clerk-react";
+  SignIn,
+  SignUp,
+  RedirectToSignIn 
+} from "@clerk/clerk-react";
 import Home from "./components/pages/home";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import Initial from "./components/pages/initial";
 
 if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
 const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
 
+function ClerkProviderWithRoutes() {
+  const navigate = useNavigate();
+
+  return (
+    <ClerkProvider
+      publishableKey={clerkPubKey}
+      navigate={(to) => navigate(to)}
+    >
+      <Routes>
+        <Route path="/" element={<Initial />} />
+        <Route
+          path="/sign-in/*"
+          element={<SignIn routing="path" path="/sign-in"/>}
+        />
+        <Route
+          path="/sign-up/*"
+          element={<SignUp routing="path" path="/sign-up" />}
+        />
+        <Route
+          path="/home"
+          element={
+          <>
+            <SignedIn>
+              <Home />
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
+          </>
+          }
+        />
+      </Routes>
+    </ClerkProvider>
+  );
+}
+
 function App() {
   return (
-    <ClerkProvider publishableKey={clerkPubKey}>
-      <SignedIn>
-        <Home />
-      </SignedIn>
-      <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
-    </ClerkProvider>
+    <BrowserRouter>
+      <ClerkProviderWithRoutes />
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/pages/initial/index.tsx
@@ -0,0 +1,8 @@
+export default function Initial() {
+  return (
+    <>
+      <h1>Public page</h1>
+      <a href="/home">Go to protected page</a>
+    </>
+  );
+}
