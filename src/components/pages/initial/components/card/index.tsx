5c14668e060b1e177628a5dcc13e478994ff61b9
Viviane Viana Bresolin <vivianebresolin@gmail.com>
2023-06-23 20:15:36 -0400

    Adjusts in layout

--- src/components/pages/initial/components/card/index.tsx
+++ src/components/pages/initial/components/card/index.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 function Card({text, css}: CardProps) {
   return ( 
-    <div className={`bg-white rounded-md shadow py-4 px-2 md:px-6 flex-1 md:flex md:items-center md:h-48 ${css}`}>
+    <div className={`border-solid border-2 border-[#03045E] bg-white bg-opacity-80 rounded-md shadow-lg py-4 px-2 md:px-6 flex-1 md:flex md:items-center md:h-48 ${css}`}>
       <AppParagraph otherCss="sm:text-lg">{text}</AppParagraph>
     </div>
   );
--- src/components/pages/initial/components/sectionTwo/index.tsx
+++ src/components/pages/initial/components/sectionTwo/index.tsx
@@ -4,10 +4,10 @@ import AppParagraph from "../../../../AppParagraph";
 function SectionTwo() {
   return ( 
     <section className="px-4 py-8 md:py-12 lg:py-20 flex flex-col gap-6 md:gap-8 lg:gap-12 bg-[#0096C7]">
-      <h2 className="text-2xl sm:text-3xl md:text-5xl text-center text-[#FE9D02]">Get Started</h2>
+      <h2 className="text-3xl sm:text-4xl md:text-5xl text-center text-[#FE9D02]">Get Started</h2>
       <AppParagraph otherCss="sm:w-1/2 sm:text-lg md:text-2xl mx-auto">Embark on your journey and experience the power of Clerk. Your authentication adventure starts now!</AppParagraph>
       <Link to="/sign-up" className="sm:mx-auto">
-        <button className="w-full sm:w-auto sm:text-lg md:text-2xl bg-white rounded-md shadow py-2 px-6">
+        <button className="w-full sm:w-auto text-lg md:text-2xl font-bold border-solid border-4 border-[#03045E] bg-white rounded-md shadow-lg py-2 px-6 md:px-8 lg:px-10">
           Sign Up
         </button>
       </Link>
@@ -22,3 +22,7 @@ function SectionTwo() {
 }
 
 export default SectionTwo;
+
+
+//#03045E - azul escuro
+//#033E8A - azul
